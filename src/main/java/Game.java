030d610af1fb2e73f12fdc9eb49593ea5d13e8a1
diogobabo <diogo.babo@gmail.com>
2022-01-02 16:58:58 +0000

    Second Commit

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,31 @@
-public class Game {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Game implements KeyListener {
+
+    public Game(){
+        //create interface
+    }
+    public void start(){
+        System.out.println("ta a andar");
+        //start game
+    }
+    private void update(){
+        //updates
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
