39fbb017db14c51496982497f160b7122dbd4dd1
RicardoCavalheiro123 <ricardo18-4-2001@hotmail.com>
2022-01-07 12:02:55 +0000

    fixed tests

--- src/test/groovy/ElementTest.groovy
+++ src/test/groovy/ElementTest.groovy
@@ -3,70 +3,80 @@ import com.aor.Positions.Position
 import spock.lang.Specification
 
 class ElementTest extends Specification {
-    private h;
+    private h
 
     def setup(){
-        h = new Hero(2,3);
+        h = new Hero(2,3)
     }
 
     def "GetPosition"() {
         when:
-            def pos = new Position(2,3);
+            def pos = new Position(2,3)
 
         then:
-            pos == h.getPosition();
+            pos == h.getPosition()
 
     }
 
     def "SetPosition"() {
         given:
-            def pos = new Position(5,4);
-            h.setPosition(pos);
+            def pos = new Position(5,4)
+            h.setPosition(pos)
 
         when:
-            def posf = new Position(5,4);
+            def posf = new Position(5,4)
 
         then:
-            posf == h.getPosition();
+            posf == h.getPosition()
     }
 
     def "moveUp"() {
         given:
-            h.moveUp();
+            h.moveUp()
         when:
-            def posf = new Position(2,2);
+            def posf = new Position(2,2)
 
         then:
-        posf == h.getPosition();
+        posf == h.getPosition()
     }
 
     def "moveDown"() {
         given:
-            h.moveDown();
+            h.moveDown()
         when:
-            def posf = new Position(2,4);
+            def posf = new Position(2,4)
 
         then:
-            posf == h.getPosition();
+            posf == h.getPosition()
     }
 
     def "moveLeft"() {
         given:
-            h.moveLeft();
+            h.moveLeft()
         when:
-            def posf = new Position(1,3);
+            def posf = new Position(1,3)
 
         then:
-            posf == h.getPosition();
+            posf == h.getPosition()
     }
 
     def "moveRight"() {
         given:
-            h.moveRight();
+            h.moveRight()
         when:
-            def posf = new Position(3,3);
+            def posf = new Position(3,3)
 
         then:
-            posf == h.getPosition();
+            posf == h.getPosition()
+    }
+    def "isAlive"() {
+        expect:
+            h.isAlive() == true
+    }
+    def "setAsDeadHero"() {
+        when:
+            h.setAsDead()
+        then:
+            h.isAlive() == false
     }
 }
